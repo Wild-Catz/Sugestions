f3c3595e5c1769be816561758881f4d932bf04c2
Горбенко Роман <45801227+rofle100lvl@users.noreply.github.com>
2023-02-16 14:09:06 +0100

    WC-1 lint errors

--- Activities/ContentView.swift
+++ Activities/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Image(systemName: "heart.fill")
+            
+            
+            
+            
+            
+            
         }
         .padding()
     }
