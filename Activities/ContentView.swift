a23205eda5e7deebe220d4e0b40b286e57ef5399
Горбенко Роман <45801227+rofle100lvl@users.noreply.github.com>
2023-02-16 11:31:25 +0100

    Build error

--- Activities/ContentView.swift
+++ Activities/ContentView.swift
@@ -8,19 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    
     var body: some View {
-        
-        
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large).foregroundColor(.accentColor)
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .cat()
         }
         .padding()
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
