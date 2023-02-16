6324bd23bde5e7ca668567fe9cc1cf6bec3c09bd
Горбенко Роман <45801227+rofle100lvl@users.noreply.github.com>
2023-02-16 08:27:02 +0100

    Xcode project init

---
+++ Activities/ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesApp.swift
+//  Activities
+//
+//  Created by Roman Gorbenko on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
